9057258f120b136adde72151fa5746a9c0202e6f
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2019-03-17 15:02:11 +0000

    Improved base structure.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,29 @@
 #include <QCoreApplication>
+#include <QDebug>
+#include <QDir>
+#include <QStandardPaths>
+#include <QString>
+
+#include <iostream>
+#include <windows.h>
+
+static HHOOK hHook = nullptr;
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     QCoreApplication app(argc, argv);
+
+    QString outputPath = QStandardPaths::writableLocation(QStandardPaths::AppDataLocation);
+    QDir outputDir = QDir(outputPath);
+    outputDir.cdUp();
+
+    qDebug() << outputDir.absolutePath();
+
+    if (hHook == nullptr) {
+        return 1;
+    }
 
     return app.exec();
 }
